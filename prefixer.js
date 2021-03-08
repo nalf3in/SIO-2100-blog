1909497b6063df2992920f0acaaf9179a3a45762
Jonathan Simard <nalf3in@gmail.com>
2021-03-08 17:25:37 -0500

    Update to try to fix the links

---
+++ prefixer.js
@@ -0,0 +1,15 @@
+const projectName = "/SIO-2100-blog/";
+const user = "https://nalf3in.github.io";
+
+function prefixLinks() {
+  let links = document.links;
+  for (var i = 0, iLen = links.length; i < iLen; i++) {
+    // use of \/\/ is to avoid SO posting rules
+    const currentLink = links[i].href;
+
+    if (currentLink.indexOf(user) != -1) {
+      links[i].href = links[i].href.replace(user, user + projectName);
+    }
+    //links[i].href = prefix + links[i].href;
+  }
+}
