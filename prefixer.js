abe2b1e07861633ece9f62f509b2ee39344f526d
Jonathan Simard <nalf3in@gmail.com>
2021-03-08 17:30:37 -0500

    Update prefixer.js

--- prefixer.js
+++ prefixer.js
@@ -2,7 +2,6 @@ const projectName = "/SIO-2100-blog";
 const user = "https://nalf3in.github.io";
 
 function prefixLinks() {
-  console.log("it works !")
   let links = document.links;
   for (var i = 0, iLen = links.length; i < iLen; i++) {
     // use of \/\/ is to avoid SO posting rules
@@ -11,6 +10,5 @@ function prefixLinks() {
     if (currentLink.indexOf(user) != -1) {
       links[i].href = links[i].href.replace(user, user + projectName);
     }
-    //links[i].href = prefix + links[i].href;
   }
 }
